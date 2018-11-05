9eb99f053d4c0454d542506ae926e076e6f59981
donald serafim <donald.serafim@portalunimed.com.br>
2018-11-05 17:02:58 -0200

    alteração + parametros

--- ex/classComponent.jsx
+++ ex/classComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ClassComponent extends Component {
     render(){
         return (
-            <h1>{this.props.value}</h1>
+            <h1>{this.props.value} - {this.props.texto}</h1>
         )
     }
 }
